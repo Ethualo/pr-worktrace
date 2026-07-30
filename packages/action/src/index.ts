d7516c2d68319f71139c90277533c5eb5b52169f
Ethualo <lkh0926ym@naver.com>
2026-07-30 16:07:11 +0900

    feat(action): add package barrel export

--- packages/action/src/index.ts
+++ packages/action/src/index.ts
@@ -1,1 +1,8 @@
-export {};
+export { loadConfig } from "./loadConfig.js";
+export type { LoadConfigParams } from "./loadConfig.js";
+export { runReview } from "./runReview.js";
+export type { RunReviewParams, RunReviewResult } from "./runReview.js";
+export { requestReasonForRejections } from "./requestReasonForRejections.js";
+export type { RequestReasonForRejectionsParams } from "./requestReasonForRejections.js";
+export { runWorklogCommit } from "./runWorklogCommit.js";
+export type { RunWorklogCommitParams } from "./runWorklogCommit.js";
